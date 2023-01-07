4d638cb5828bb5d172941ee80f1a1891e6240d0f
Chi Chen <stephchen55@gmail.com>
2023-01-07 22:07:47 +0800

    新增開場白script + add assets

---
+++ Assets/Scripts/Now/Begin_Story.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Begin_Story : MonoBehaviour
+{
+    // 開場獨白 UI
+    public GameObject intro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 按下 space 開始此關卡
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            intro.SetActive(false);
+
+            gameObject.GetComponent<GameManager>().enabled = true;
+        }
+        
+    }
+}
